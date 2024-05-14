d873465d985aba269a6dfe89a021b9e1fbcccb08
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-05-14 13:06:13 +0500

    replace classes with  arbitrary values

--- src/components/home/hero/SearchBar.jsx
+++ src/components/home/hero/SearchBar.jsx
@@ -1,24 +1,24 @@
 const SearchBar = () => {
   return (
-    <div className="flex flex-col sm:flex-row items-center justify-between gap-2 sm:bg-gray bg-transparent rounded-full p-[10px] w-full sm:w-[95%] mx-auto lg:max-w-[800px]">
-      <div className="bg-white w-full rounded-full sm:w-[40%] lg:w-[45%] flex items-center gap-6 px-10 sm:px-5 md:px-16">
-        <h3 className="text-2xl leading-[24px]">Find</h3>
+    <div className="flex flex-col sm:flex-row items-center justify-between gap-2 sm:bg-gray bg-transparent rounded-full p-2.5 w-full sm:w-95 mx-auto lg:max-w-800">
+      <div className="bg-white w-full rounded-full sm:w-40 lg:w-45 flex items-center gap-6 px-10 sm:px-5 md:px-16">
+        <h3 className="text-2xl leading-24">Find</h3>
         <input
-          className="py-4 leading-[20px] w-full bg-transparent outline-none text-xl"
+          className="py-4 leading-20 w-full bg-transparent outline-none text-xl"
           type="text"
           placeholder="Ex: workspace"
         />
       </div>
-      <div className="hidden sm:block w-[1px] bg-gray min-h-[40px]" />
-      <div className="bg-white w-full rounded-full sm:w-[40%] lg:w-[45%] flex items-center gap-6 px-10 sm:px-5 md:px-16">
-        <h3 className="text-2xl leading-[24px]">Where</h3>
+      <div className="hidden sm:block w-1 bg-gray min-h-20" />
+      <div className="bg-white w-full rounded-full sm:w-40 lg:w-45 flex items-center gap-6 px-10 sm:px-5 md:px-16">
+        <h3 className="text-2xl leading-24">Where</h3>
         <input
-          className="py-4 w-full bg-transparent outline-none leading-[20px] text-xl"
+          className="py-4  bg-transparent outline-none leading-20 text-xl"
           type="text"
           placeholder="Your City"
         />
       </div>
-      <div className="w-[60px] h-[60px] rounded-full bg-primary flex items-center justify-center cursor-pointer transition-all hover:opacity-60">
+      <div className="w-20 h-20 rounded-full bg-primary flex items-center justify-center cursor-pointer transition-all hover:opacity-60">
         <img src="/images/icons/search.png" alt="search" />
       </div>
     </div>
--- src/components/home/hero/index.jsx
+++ src/components/home/hero/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import './Herostyle.css';
 
 export default function Hero() {
   return (
     <section className="mt-4">
-      {/* style={{ backgroundImage: 'url(/images/hero-banner.png)' }} */}
-      <div className="container min-h-[85vh] bg-no-repeat bg-cover rounded-[32px] py-20 flex flex-col justify-center items-center text-center">
-        <h2 className="text-[40px] leading-[50px] sm:text-[60px] sm:leading-[72px] md:text-[70px] md:leading-[82px] lg:text-[82px] lg:leading-[95px] text-black">
+      <div className="container min-h-screen bg-no-repeat bg-cover rounded-32 py-20 flex flex-col justify-center items-center text-center">
+        {/* trial to check class working */}
+        <div className="rounded-32">Content with 32px border radius</div>
+
+        <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl text-black leading-60 sm:leading-72 md:leading-82 lg:leading-95">
           Find Your Next <br />
           Coworking Office
         </h2>
-        <h4 className="text-[30px] leading-[30px] sm:text-[35px] sm:leading-[35px] text-black font-normal mt-6 mb-10">
+        <h4 className="text-xl sm:text-2xl text-black leading-35 font-normal mt-6 mb-10">
           Explore the workspaces
         </h4>
 
         <SearchBar />
-        <p className="text-white mt-9 font-medium text-[22px]">
+        <p className="text-white mt-9 font-medium text-xl">
           The World's Largest Coworking Space Marketplace | Search & Get Quotes
           Today
         </p>
@@ -23,5 +26,3 @@ export default function Hero() {
     </section>
   );
 }
-
-// export default Hero;
