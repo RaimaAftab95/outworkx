113a4eb3f8553ab859ce0e280916ed4536cc3cf5
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-05-15 16:11:23 +0500

    spaces reduced search btn size dec

--- src/components/home/hero/SearchBar.jsx
+++ src/components/home/hero/SearchBar.jsx
@@ -1,7 +1,7 @@
 export default function SearchBar() {
   return (
-    <div className="flex flex-col sm:flex-row items-center justify-between gap-2 sm:bg-gray bg-transparent rounded-full p-2.5 w-full sm:max-w-24 mx-auto lg:max-w-4xl">
-      <div className="bg-white w-full rounded-full sm:w-5/12 lg:w-11/24 flex items-center gap-6 px-10 sm:px-5 md:px-16">
+    <div className="flex flex-col sm:flex-row items-center justify-center  gap-2 sm:bg-gray bg-transparent rounded-full p-2.5 w-full sm:max-w-24 mx-auto lg:max-w-4xl">
+      <div className="bg-white w-full rounded-full sm:w-5/12 lg:w-11/24 flex items-center gap-6  px-10 sm:px-5 md:px-16">
         <h3 className="text-2xl leading-6">Find</h3>
         <input
           className="py-4 leading-6 w-full bg-transparent outline-none text-xl"
@@ -9,7 +9,7 @@ export default function SearchBar() {
           placeholder="Ex: workspace"
         />
       </div>
-      <div className="hidden sm:block w-px bg-gray min-h-10" />
+      <div className="hidden sm:block bg-gray min-h-10" />
       <div className="bg-white w-full rounded-full sm:w-5/12 lg:w-11/24 flex items-center gap-6 px-10 sm:px-5 md:px-10">
         <h3 className="text-2xl leading-6">Where</h3>
         <input
