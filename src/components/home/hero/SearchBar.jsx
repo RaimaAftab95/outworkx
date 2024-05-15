7b593e66f808e2ba49141b1e0077cc7cf549a0f1
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-05-15 11:17:58 +0500

    remove all [] and additional css searchbar function also chnge to  export

--- src/components/home/hero/SearchBar.jsx
+++ src/components/home/hero/SearchBar.jsx
@@ -1,7 +1,7 @@
-const SearchBar = () => {
+export default function SearchBar() {
   return (
     <div className="flex flex-col sm:flex-row items-center justify-between gap-2 sm:bg-gray bg-transparent rounded-full p-2.5 w-full sm:max-w-24 mx-auto lg:max-w-4xl">
-      <div className="bg-white w-full rounded-full sm:w-[40%] lg:w-[45%] flex items-center gap-6 px-10 sm:px-5 md:px-16">
+      <div className="bg-white w-full rounded-full sm:w-5/12 lg:w-11/24 flex items-center gap-6 px-10 sm:px-5 md:px-16">
         <h3 className="text-2xl leading-6">Find</h3>
         <input
           className="py-4 leading-5 w-full bg-transparent outline-none text-xl"
@@ -9,8 +9,8 @@ const SearchBar = () => {
           placeholder="Ex: workspace"
         />
       </div>
-      <div className="hidden sm:block w-px bg-gray min-h-[40px]" />
-      <div className="bg-white w-full rounded-full sm:w-[40%] lg:w-[45%] flex items-center gap-6 px-10 sm:px-5 md:px-16">
+      <div className="hidden sm:block w-px bg-gray min-h-10" />
+      <div className="bg-white w-full rounded-full sm:w-5/12 lg:w-11/24 flex items-center gap-6 px-10 sm:px-5 md:px-16">
         <h3 className="text-2xl leading-6">Where</h3>
         <input
           className="py-4 w-full bg-transparent outline-none leading-5 text-xl"
@@ -23,5 +23,4 @@ const SearchBar = () => {
       </div>
     </div>
   );
-};
-export default SearchBar;
+}
--- src/components/home/hero/index.jsx
+++ src/components/home/hero/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-import './Herostyle.css';
 
 export default function Hero() {
   return (
     <section className="mt-4">
       <div className="container min-h-screen bg-no-repeat bg-cover rounded-32 py-20 flex flex-col justify-center items-center text-center">
-        {/* trial to check class working */}
-        <div className="rounded-32">Content with 32px border radius</div>
-
         <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl text-black leading-60 sm:leading-72 md:leading-82 lg:leading-95">
           Find Your Next <br />
           Coworking Office
