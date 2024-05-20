58d651de6acb32cb3320a3152be77d5af8fc0723
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-05-20 19:17:03 +0500

    resolve conflict

--- src/components/home/hero/SearchBar.jsx
+++ src/components/home/hero/SearchBar.jsx
@@ -1,19 +1,19 @@
 export default function SearchBar() {
   return (
-    <div className="flex flex-col sm:flex-row items-center justify-center  gap-2 sm:bg-gray bg-transparent rounded-full p-2.5 w-full sm:max-w-24 mx-auto lg:max-w-4xl">
-      <div className="bg-white w-full rounded-full sm:w-5/12 lg:w-11/24 flex items-center gap-6  px-10 sm:px-5 md:px-16">
-        <h3 className="text-2xl leading-6">Find</h3>
+    <div className="flex flex-col sm:flex-row items-center justify-center  gap-2 sm:bg-gray rounded-full p-2.5 w-full sm:max-w-24 mx-auto lg:max-w-4xl">
+      <div className="bg-white w-full rounded-full sm:w-5/12 lg:w-3/7 flex items-center gap-6  px-10 sm:px-5 md:px-16">
+        <h3 className="text-2xl">Find</h3>
         <input
-          className="py-4 leading-6 w-full bg-transparent outline-none text-xl"
+          className="py-4 w-full outline-none text-xl"
           type="text"
           placeholder="Ex: workspace"
         />
       </div>
       <div className="hidden sm:block bg-gray min-h-10" />
-      <div className="bg-white w-full rounded-full sm:w-5/12 lg:w-11/24 flex items-center gap-6 px-10 sm:px-5 md:px-10">
-        <h3 className="text-2xl leading-6">Where</h3>
+      <div className="bg-white w-full rounded-full sm:w-5/12 lg:w-3/7 flex items-center gap-6 px-10 sm:px-5 md:px-10">
+        <h3 className="text-2xl">Where</h3>
         <input
-          className="py-4 w-full bg-transparent outline-none leading-6 text-xl"
+          className="py-4 w-full outline-none text-xl"
           type="text"
           placeholder="Your City"
         />
