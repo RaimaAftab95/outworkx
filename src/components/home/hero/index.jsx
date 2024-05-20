29b4a4ab688a6bcb1e15a82578b076a990e17d83
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-05-20 19:36:55 +0500

    utilize-object fit conflict resolve

--- src/components/home/hero/index.jsx
+++ src/components/home/hero/index.jsx
@@ -5,16 +5,16 @@ export default function Hero() {
   return (
     <section>
       <div className="container min-h-screen rounded-32 py-20 flex flex-col justify-center items-center text-center">
-        <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl text-black leading-60 sm:leading-72 md:leading-82 lg:leading-95">
+        <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl">
           Find Your Next <br />
           Coworking Office
         </h2>
-        <h4 className="text-xl sm:text-2xl text-black leading-35 font-normal mt-6 mb-10">
+        <h4 className="text-xl sm:text-2xl font-normal mt-6 mb-10">
           Explore the workspaces
         </h4>
 
         <SearchBar />
-        <p className="text-white mt-9 font-medium text-xl">
+        <p className="mt-9 font-medium text-xl">
           The World's Largest Coworking Space Marketplace | Search & Get Quotes
           Today
         </p>
