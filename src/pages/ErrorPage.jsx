daa631584f7ed9b480f9b4d5cc3559ffe8514e8d
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-06-12 12:53:48 +0500

    CU-86epjhdbe-sort-issues

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
 
-const ErrorPage = () => {
-  const error = useRouteError();
-  const errorMessage =
-    error?.statusText ||
-    error?.message ||
-    "This page doesn't exist. Please enter a valid path.";
-
+export default function ErrorPage() {
   return (
     <div className="bg-gray-100 flex min-h-screen items-center justify-center p-4">
       <div className="w-full max-w-lg rounded-lg bg-white p-8 text-center shadow-md">
         <h1 className="mb-8 text-3xl font-bold">Oops! 👋</h1>
-        <p className="mb-4 text-lg text-black">
-          Sorry, an unexpected error has occurred.
-        </p>
+        <p className="mb-4 text-lg uppercase text-black">404-Page not found.</p>
         <p className="text-gray-500 text-sm">
-          <i>{errorMessage}</i>
+          <i>We are sorry but the Page you requested does not exist</i>
         </p>
       </div>
     </div>
   );
-};
-
-export default ErrorPage;
+}
