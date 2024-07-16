8510c904d9d8722cf145e75818bbabdad160b925
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-07-16 16:00:02 +0500

    CU-86epuhk7w-make-bookings-Functional changes

--- src/context/CreateBookingContext.jsx
+++ src/context/CreateBookingContext.jsx
@@ -1,50 +1,96 @@
+// import { createContext, useReducer } from 'react';
+
+// export const CreateBookingContext = createContext();
+
+// const initialState = {
+//   booking: null,
+//   isLoading: false,
+//   error: null
+// };
+
+// const bookingReducer = (state, action) => {
+//   switch (action.type) {
+//     case 'BOOKING_REQUEST':
+//       return {
+//         ...state,
+//         isLoading: true,
+//         error: null
+//       };
+//     case 'BOOKING_SUCCESS':
+//       return {
+//         ...state,
+//         booking: action.payload.booking,
+//         isLoading: false
+//       };
+//     case 'BOOKING_FAILURE':
+//       return {
+//         ...state,
+//         isLoading: false,
+//         error: action.payload.error
+//       };
+//     case 'CLEAR_BOOKING':
+//       return {
+//         ...state,
+//         booking: null,
+//         error: null
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export const CreateBookingContextProvider = ({ children }) => {
+//   const [state, dispatch] = useReducer(bookingReducer, initialState);
+
+//   return (
+//     <CreateBookingContext.Provider value={{ state, dispatch }}>
+//       {children}
+//     </CreateBookingContext.Provider>
+//   );
+// };
+
 import { createContext, useReducer } from 'react';
 
 export const CreateBookingContext = createContext();
 
-const initialState = {
-  booking: null,
-  isLoading: false,
-  error: null
-};
+/**
+ * @typedef {Object} Booking Booking Entity
+ * @property {string} spaceId Space ID
+ * @property {string} startDate Booking start date
+ * @property {string} endDate Booking end date
+ * @property {number} price Total booking price
+ * @property {number} numberOfDesks Number of desks booked
+ * @property {string} status Booking status
+ */
 
-const bookingReducer = (state, action) => {
+/** @type {Booking | Object} */
+const initialState = {};
+
+export function createBookingReducer(state, action) {
   switch (action.type) {
-    case 'BOOKING_REQUEST':
+    case 'SET_BOOKING_DETAILS':
       return {
         ...state,
-        isLoading: true,
-        error: null
-      };
-    case 'BOOKING_SUCCESS':
-      return {
-        ...state,
-        booking: action.payload.booking,
-        isLoading: false
-      };
-    case 'BOOKING_FAILURE':
-      return {
-        ...state,
-        isLoading: false,
-        error: action.payload.error
-      };
-    case 'CLEAR_BOOKING':
-      return {
-        ...state,
-        booking: null,
-        error: null
+        spaceId: action.payload.spaceId,
+        startDate: action.payload.startDate,
+        endDate: action.payload.endDate,
+        price: action.payload.price,
+        numberOfDesks: action.payload.numberOfDesks,
+        status: action.payload.status
       };
     default:
       return state;
   }
-};
+}
 
-export const CreateBookingContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(bookingReducer, initialState);
+export function CreateBookingContextProvider({ children }) {
+  const [state, dispatch] = useReducer(createBookingReducer, initialState);
+
+  console.log('Booking context state', state);
 
   return (
     <CreateBookingContext.Provider value={{ state, dispatch }}>
       {children}
     </CreateBookingContext.Provider>
   );
-};
+}
